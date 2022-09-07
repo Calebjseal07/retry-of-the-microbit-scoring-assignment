2484db409ffb12b5e1d3728789c4dec9f5e9f549
Calebjseal07 <113064908+Calebjseal07@users.noreply.github.com>
2022-09-07 15:09:17 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Keefe += 1
+    basic.showString("Keefe Score")
+    basic.showString("" + (Keefe))
+    basic.clearScreen()
 })
+input.onButtonPressed(Button.AB, function () {
+    Tie += 1
+    basic.showString("Tie Total")
+    basic.showString("" + (Tie))
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    Fitz += 1
+    basic.showString("Fitz Score")
+    basic.showString("" + (Fitz))
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Keefe Score")
+    basic.showString("" + (Keefe))
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showString("Fitz Score")
+    basic.showString("" + (Fitz))
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showString("Tie Total")
+    basic.showString("" + (Tie))
+    basic.clearScreen()
+    basic.pause(500)
+})
+let Tie = 0
+let Fitz = 0
+let Keefe = 0
+Keefe = 0
+Fitz = 0
+Tie = 0
